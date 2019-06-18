7aff1c3f5a23c6610f9e7d872eec52b91d2392a6
Charles Clemens <cclemensjr@gmail.com>
2019-06-18 12:04:06 -0700

    Added handler for homepage

--- src/main/java/com/superfamicomcoder/codefellowship/StaticController.java
+++ src/main/java/com/superfamicomcoder/codefellowship/StaticController.java
@@ -1,7 +1,8 @@
-package com.superfamicomcoder.codefellowship.controllers;
+package com.superfamicomcoder.codefellowship;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class StaticController {
