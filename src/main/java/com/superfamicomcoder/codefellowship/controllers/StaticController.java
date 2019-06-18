333221d9797a26252fe312c9213ccf9e0124cc34
Charles Clemens <cclemensjr@gmail.com>
2019-06-18 10:17:00 -0700

    Added homepage controller

--- src/main/java/com/superfamicomcoder/codefellowship/controllers/StaticController.java
+++ src/main/java/com/superfamicomcoder/codefellowship/controllers/StaticController.java
@@ -1,4 +1,13 @@
 package com.superfamicomcoder.codefellowship.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class StaticController {
+
+    @GetMapping("/")
+    public String getHomePage() {
+        return "index";
+    }
 }
