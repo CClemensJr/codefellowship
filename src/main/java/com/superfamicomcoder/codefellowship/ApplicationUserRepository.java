6b73a2acde0da611b05d9d6276cabf972bcf3a84
Charles Clemens <cclemensjr@gmail.com>
2019-06-18 12:28:20 -0700

    Implemented app user repository

---
+++ src/main/java/com/superfamicomcoder/codefellowship/ApplicationUserRepository.java
@@ -0,0 +1,7 @@
+package com.superfamicomcoder.codefellowship;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ApplicationUserRepository extends CrudRepository<ApplicationUser, Long> {
+    ApplicationUser findByUsername(String username);
+}
