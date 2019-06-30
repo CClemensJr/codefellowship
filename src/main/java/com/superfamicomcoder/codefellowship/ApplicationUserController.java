ad17be299cd2755d6b202db751dce2f0b8cd4349
Charles Clemens <cclemensjr@gmail.com>
2019-06-30 10:47:00 -0700

    Updated profile page

--- src/main/java/com/superfamicomcoder/codefellowship/ApplicationUserController.java
+++ src/main/java/com/superfamicomcoder/codefellowship/ApplicationUserController.java
@@ -8,13 +8,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
-
 import java.security.Principal;
 import java.util.ArrayList;
-import java.util.Date;
+
 
 @Controller
 public class ApplicationUserController {
